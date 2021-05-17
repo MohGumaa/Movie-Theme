11115f1fc02b0c81e24df2a750941cc68a86482c
MohGumaa <gomaam14@gmail.com>
2021-05-17 22:01:21 +0400

    Add Loop Function For Post From DB

--- page.php
+++ page.php
@@ -2,6 +2,6 @@
 
     <h1><?php the_title();?></h1>
 
-    <?php get_template_part('template-part/section', 'content');?>
+    <?php get_template_part('template-part/page/section', 'content');?>
 
 <?php get_footer(); ?>
